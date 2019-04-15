c0ebc9d2726508a348d1576cc94578dbaca10ce3
yanxiaofei <forget_love2161@126.com>
2019-04-16 00:57:51 +0800

    feat: 添加保存功能页面

--- ssm_web/src/main/java/com/isyxf/ssm/controller/DepartmentController.java
+++ ssm_web/src/main/java/com/isyxf/ssm/controller/DepartmentController.java
@@ -16,12 +16,32 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+
     // /department/list.do
     public void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Department> list = departmentService.getAll();
-//        List list = new ArrayList();
-
         request.setAttribute("LIST", list);
         request.getRequestDispatcher("../department_list.jsp").forward(request, response);
+    }
+
+    // /department/add.do
+    public void toAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("../department_add.jsp").forward(request, response);
+    }
+
+    // 结束添加
+    public void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String address = request.getParameter("address");
+
+        Department department = new Department();
+
+        department.setName(name);
+        department.setAddress(address);
+
+        departmentService.add(department);
+
+        // 回控制器
+        response.sendRedirect("list.do");
     }
 }
