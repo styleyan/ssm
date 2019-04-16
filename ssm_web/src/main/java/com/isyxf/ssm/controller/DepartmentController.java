4ba3cc80d289f70d7e6be485dcc4d69207e77287
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-04-16 21:30:11 +0800

    feat: 增加修改信息功能

--- ssm_web/src/main/java/com/isyxf/ssm/controller/DepartmentController.java
+++ ssm_web/src/main/java/com/isyxf/ssm/controller/DepartmentController.java
@@ -29,7 +29,13 @@ public class DepartmentController {
         request.getRequestDispatcher("../department_add.jsp").forward(request, response);
     }
 
-    // 结束添加
+    /**
+     * 保存数据
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     public void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
         String address = request.getParameter("address");
@@ -40,6 +46,37 @@ public class DepartmentController {
         department.setAddress(address);
 
         departmentService.add(department);
+
+        // 回控制器
+        response.sendRedirect("list.do");
+    }
+
+    /**
+     * 编辑
+     * /department/toEdit.do
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void toEdit(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("../department_edit.jsp").forward(httpServletRequest, httpServletResponse);
+    }
+
+    /**
+     * 编辑信息
+     */
+    public void edit(HttpServletRequest httpServletRequest, HttpServletResponse response) throws ServletException, IOException {
+        String name = httpServletRequest.getParameter("name");
+        String address = httpServletRequest.getParameter("address");
+        String id = httpServletRequest.getParameter("id");
+        Department department = new Department();
+
+        department.setAddress(address);
+        department.setName(name);
+        department.setId(Integer.parseInt(id));
+
+        departmentService.edit(department);
 
         // 回控制器
         response.sendRedirect("list.do");
