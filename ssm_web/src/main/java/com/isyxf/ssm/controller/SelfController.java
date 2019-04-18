b0769a4fc8d99ff2cee27b23eaf9b0e8e6696246
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-04-18 14:31:27 +0800

    feat: 完善修改密码功能

--- ssm_web/src/main/java/com/isyxf/ssm/controller/SelfController.java
+++ ssm_web/src/main/java/com/isyxf/ssm/controller/SelfController.java
@@ -49,4 +49,29 @@ public class SelfController {
     public void main(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
+
+    // /self/info.do
+    public void info(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("../info.jsp").forward(request, response);
+    }
+
+    // /self/toChangePassword.do
+    public void toChangePassword(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("../change_password.jsp").forward(request, response);
+    }
+
+    // /self/changePassword.do
+    public void changePassword(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String password = request.getParameter("password");
+        String password1 = request.getParameter("password1");
+        HttpSession session = request.getSession();
+        Staff staff = (Staff)session.getAttribute("USER");
+
+        if (!staff.getPassword().equals(password)) {
+            response.sendRedirect("toChangePassword.do");
+        } else {
+            selfService.changePassword(staff.getId(), password1);
+            response.getWriter().println("<script type=\"text/javascript\">parent.location.href=\"../logout.do\"</script>");
+        }
+    }
 }
