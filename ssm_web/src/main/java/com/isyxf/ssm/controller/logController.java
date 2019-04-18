9a079989ca0a5747566b35a0cb95fdbc39520af5
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-04-18 17:16:14 +0800

    feat: 增加日志统计

---
+++ ssm_web/src/main/java/com/isyxf/ssm/controller/logController.java
@@ -0,0 +1,42 @@
+package com.isyxf.ssm.controller;
+
+
+import com.isyxf.ssm.entity.Log;
+import com.isyxf.ssm.service.LogService;
+import com.sun.deploy.net.HttpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller("logController")
+public class logController {
+
+    @Autowired
+    private LogService logService;
+
+    public void operationLog(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Log> list = logService.getOperationLog();
+        request.setAttribute("LIST", list);
+        request.setAttribute("TYPE", "操作");
+        request.getRequestDispatcher("../log_list.jsp").forward(request, response);
+    }
+
+    public void loginLog(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Log> list = logService.getLoginLog();
+        request.setAttribute("LIST", list);
+        request.setAttribute("TYPE", "登录");
+        request.getRequestDispatcher("../log_list.jsp").forward(request, response);
+    }
+
+    public void systemLog(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Log> list = logService.getSystemLog();
+        request.setAttribute("LIST", list);
+        request.setAttribute("TYPE", "系统");
+        request.getRequestDispatcher("../log_list.jsp").forward(request, response);
+    }
+}
---
+++ ssm_web/src/main/java/com/isyxf/ssm/global/LogAdvice.java
@@ -0,0 +1,106 @@
+package com.isyxf.ssm.global;
+
+import com.isyxf.ssm.entity.Log;
+import com.isyxf.ssm.entity.Staff;
+import com.isyxf.ssm.service.LogService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Component
+@Aspect
+public class LogAdvice {
+    @Autowired
+    private LogService logService;
+
+    /**
+     * 操作日志 方法执行后都进行一次记录操作
+     * @param joinPoint
+     */
+    @After("execution(* com.isyxf.ssm.controller.*.*(..)) && !execution(* com.isyxf.ssm.controller.SelfController.*(..)) && !execution(* com.isyxf.ssm.controller.SelfController.*.to*(..))")
+    private void operationLog(JoinPoint joinPoint) {
+         Log log = new Log();
+         log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+         log.setOperation(joinPoint.getSignature().getName());
+
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+
+        Object obj = session.getAttribute("USER");
+        Staff staff = (Staff)obj;
+
+        log.setOperation(staff.getAccount());
+        log.setResult("成功");
+
+        logService.addOperationLog(log);
+    }
+
+    /**
+     * 系统日志 方法错误后都进行一次记录操作
+     * @param joinPoint
+     */
+    @AfterThrowing(throwing = "e", pointcut = "execution(* com.isyxf.ssm.controller.*.*(..)) && !execution(* com.isyxf.ssm.controller.SelfController.*(..))")
+    private void systemLog(JoinPoint joinPoint, Throwable e) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+
+        Object obj = session.getAttribute("USER");
+        Staff staff = (Staff)obj;
+
+        log.setOperation(staff.getAccount());
+        log.setResult(e.getClass().getSimpleName());
+
+        logService.addSystemLog(log);
+    }
+
+    /**
+     * 登录日志 方法执行后都进行一次记录操作
+     * @param joinPoint
+     */
+    @After("execution(* com.isyxf.ssm.controller.SelfController.login(..))")
+    private void loginLog(JoinPoint joinPoint) {
+        this.log(joinPoint);
+    }
+
+    /**
+     * 退出日志 方法执行后都进行一次记录操作
+     * @param joinPoint
+     */
+    @Before("execution(* com.isyxf.ssm.controller.SelfController.logout(..))")
+    private void logoutLog(JoinPoint joinPoint) {
+        this.log(joinPoint);
+    }
+
+    private void log(JoinPoint joinPoint) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+
+        Object obj = session.getAttribute("USER");
+
+        if (obj == null) {
+            log.setOperation(request.getParameter("account"));
+            log.setResult("失败");
+        }else {
+            Staff staff = (Staff)obj;
+            log.setOperation(staff.getAccount());
+            log.setResult("成功");
+        }
+
+        logService.addLoginLog(log);
+    }
+}
